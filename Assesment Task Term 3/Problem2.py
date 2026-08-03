c32eda812d347a706e60acda9642e9a7706f190d
tommyc09 <tacorrigan@outlook.com>
2026-08-04 09:40:05 +1000

    Code for problem 2 Completed

--- Assesment Task Term 3/Problem2.py
+++ Assesment Task Term 3/Problem2.py
@@ -1,16 +1,48 @@
+# classroom = [["𖨆", "𖨆", "0"], # 0
+#              ["0", "𖨆", "0"], # 1
+#              ["0", "𖨆", "0"]] # 2
+
+# Test Cases for count_empty
+
+# classroom=[[1,2],[3,4]] # Test case 1
+# classroom=[[1,2,0],[0,3,4],[5,0,0]] # Test case 1
+# classroom=[[0,0],[0,0]] # Test case 1
+
+
+# Test Cases for most_empty_row
+# classroom=[[1,2,0],[0,3,4],[5,0,0]] # Test case 1
+# classroom=[[1,2,3],[0,0,4],[5,0,0]] # Test case 1 
+# classroom=[[1,2],[3,4]] # Test case 1
+
+
+
 def count_empty(classroom: list) -> int:
-    count = 0
+    empty = 0
     for row in classroom:
-        # TODO: loop through each seat in this row and update count
-        pass
-    return count
- 
+        for seat in row:
+            if seat == 0:
+                empty += 1
+    
+    
+    return empty
+
 def most_empty_row(classroom: list) -> int:
     best_row = 0
     best_count = -1
+
     for row_index in range(len(classroom)):
-        # TODO: count the empty seats in classroom[row_index]
-        # TODO: if this row has more empty seats than best_count,
-        #       update best_row and best_count
-        pass
+        empty_count = 0
+        for seat in classroom[row_index]:
+            if seat == 0:
+                empty_count += 1
+
+        if empty_count > best_count:
+            best_count = empty_count
+            best_row = row_index
+
     return best_row
+
+print(f"Empty Seats: {count_empty(classroom)}")
+print(f"Most Empty Row: {most_empty_row(classroom)}")
+for class1 in classroom:
+    print(class1)
